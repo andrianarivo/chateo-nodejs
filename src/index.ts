d897b959934156a04398a407350f5c0c827abd2a
David Andrianarivo <andrianarivo.david@gmail.com>
2024-01-11 16:40:53 +0300

    chore: update hostname and logs

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
+import ip from 'ip'
+import * as process from 'process'
 import server from './server'
 import log from './log'
 
 function main() {
   server.listen(4000, () => {
-    log(`Server is running on http://172.19.0.3:4000/graphql`)
+    if (process.platform === 'win32') {
+      log(`Server is running  http://localhost:4000/graphql`)
+    } else if (process.platform === 'linux') {
+      log(`Server is running  http://${ip.address()}:4000/graphql`)
+    }
   })
 }
 
