cf6250d229a668a39d8b4fcd9349d24e1766fe39
david <andrianarivo.david@gmail.com>
2024-01-12 12:54:57 +0300

    chore: update port to be dynamic

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,12 @@ import server from './server'
 import log from './log'
 
 function main() {
-  server.listen(4000, () => {
+  const port = process.env.PORT || 4000
+  server.listen(port, () => {
     if (process.platform === 'win32') {
-      log(`Server is running  http://localhost:4000/graphql`)
+      log(`Server is running  http://localhost:${port}/graphql`)
     } else if (process.platform === 'linux') {
-      log(`Server is running  http://${ip.address()}:4000/graphql`)
+      log(`Server is running  http://${ip.address()}:${port}/graphql`)
     }
   })
 }
